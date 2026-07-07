2625ad4c185260005d37ac5d543eaa5b3c358e44
Nattapong Gontanagit <nattapong.g@kkumail.com>
2026-07-07 21:59:02 +0700

    Lab 3.1:Unit tests, target reports, and Excel template

--- LabDemo/Lab3_JUnit/junit6-labdemo/src/test/java/MyFirstJUnitJupiterTest.java
+++ LabDemo/Lab3_JUnit/junit6-labdemo/src/test/java/MyFirstJUnitJupiterTest.java
@@ -2,21 +2,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.Calculator;
 
+import sqa.lab.ShiftCipher;
+
 import org.junit.jupiter.api.Test;
 
 class MyFirstJUnitJupiterTests {
 
-	private final Calculator calculator = new Calculator();
+	/* private final Calculator calculator = new Calculator();
 
 	//TC01: firstNumber = 1, secondNumber = 1, expected result = 2
 	@Test
 	void addition() {
 		assertEquals(2, calculator.add(1, 1));
 	}
-	
+
 	//TC02: firstNumber = 1, secondNumber = 1, expected result = 3 => failed
 	@Test
 	void fail_example_addition () {
 		assertEquals(3, calculator.add(1, 1));
-	}
+	} 
+
+	*/
+
+	@Test
+    void testTC01_SoftwareUppercase() {
+        ShiftCipher cipher = new ShiftCipher();
+        assertEquals("ZVMADHYL", cipher.shift("SOFTWARE", 3));
+    }
+
+    @Test
+    void testTC02_SoftwareLowercase() {
+        ShiftCipher cipher = new ShiftCipher();
+        assertEquals("zvmadhyl", cipher.shift("software", 3));
+    }
+
+    @Test
+    void testTC03_PositiveCase() {
+        ShiftCipher cipher = new ShiftCipher();
+        assertEquals("VTD", cipher.shift("Sqa", 3));
+    }
+
+    @Test
+    void testTC04_NegativeCaseWithNumber() {
+        ShiftCipher cipher = new ShiftCipher();
+        assertEquals("VTD1", cipher.shift("Sqa1", 3));
+    }
+
 }
